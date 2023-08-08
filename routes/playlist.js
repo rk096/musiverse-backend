574518fa5c18e1fec56320566fa560ee14629bc5
richa <richakamani32@gmail.com>
2023-08-08 15:03:28 -0500

    add song to playlist

--- routes/playlist.js
+++ routes/playlist.js
@@ -47,7 +47,6 @@ router.get(
 );
 
 // Get all playlists made by an artist
-// /get/artist/xyz
 router.get(
     "/get/artist/:artistId",
     passport.authenticate("jwt", {session: false}),
@@ -64,5 +63,41 @@ router.get(
         return res.status(200).json({data: playlists});
     }
 );
+
+// Add a song to a playlist
+router.post(
+    "/add/song",
+    passport.authenticate("jwt", {session: false}),
+    async (req, res) => {
+        const currentUser = req.user;
+        const {playlistId, songId} = req.body;
+        // Get the playlist if valid
+        const playlist = await Playlist.findOne({_id: playlistId});
+        if (!playlist) {
+            return res.status(304).json({err: "Playlist does not exist"});
+        }
+
+        // Check if currentUser owns the playlist or is a collaborator
+        if (
+            !playlist.owner.equals(currentUser._id) &&
+            !playlist.collaborators.includes(currentUser._id)
+        ) {
+            return res.status(400).json({err: "Not allowed"});
+        }
+        //Check if the song is a valid song
+        const song = await Song.findOne({_id: songId});
+        if (!song) {
+            return res.status(304).json({err: "Song does not exist"});
+        }
+
+        //We can now simply add the song to the playlist
+        playlist.songs.push(songId);
+        await playlist.save();
+
+        return res.status(200).json(playlist);
+    }
+);
+
+
 
 module.exports = router;
