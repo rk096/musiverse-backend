d42a9931530a748be959ab7d158d783b5ed3ad26
richa <richakamani32@gmail.com>
2023-09-28 18:24:24 -0400

    get song by admin

--- routes/song.js
+++ routes/song.js
@@ -18,7 +18,7 @@ router.post(
                 .json({ err: "Insufficient details to create song." });
         }
         const artist = req.user._id;
-        const songDetails = { name, thumbnail, track, artist ,duration};
+        const songDetails = { name, thumbnail, track, artist, duration };
         const createdSong = await Song.create(songDetails);
         return res.status(200).json(createdSong);
     }
@@ -38,52 +38,46 @@ router.get(
 
 // Get route to get all songs any artist has published
 router.get(
-    "/get/admin/:artistId",
+    "/get/admin",
     async (req, res) => {
-        const {artistId} = req.params;
-        
-        const artist = await User.findOne({_id: artistId});
-        // ![] = false
-        // !null = true
-        // !undefined = true
+        const artist = await User.findOne({ _id: "6514369bff4cda05b3f75538" });
         if (!artist) {
-            return res.status(301).json({err: "Artist does not exist"});
+            return res.status(301).json({ err: "song does not exist" });
         }
-
-        const songs = await Song.find({artist: artistId});
-        return res.status(200).json({data: songs});
+        const songs = await Song.find({ artist: "6514369bff4cda05b3f75538" });
+        return res.status(200).json({ data: songs });
     }
 );
 
 // Get route to get all songs any artist has published
 router.get(
     "/get/artist/:artistId",
-    passport.authenticate("jwt", {session: false}),
+    passport.authenticate("jwt", { session: false }),
     async (req, res) => {
-        const {artistId} = req.params;
-        
-        const artist = await User.findOne({_id: artistId});
+        const { artistId } = req.params;
+
+        const artist = await User.findOne({ _id: artistId });
         // ![] = false
         // !null = true
         // !undefined = true
         if (!artist) {
-            return res.status(301).json({err: "Artist does not exist"});
+            return res.status(301).json({ err: "Artist does not exist" });
         }
 
-        const songs = await Song.find({artist: artistId});
-        return res.status(200).json({data: songs});
+        const songs = await Song.find({ artist: artistId });
+        return res.status(200).json({ data: songs });
     }
 );
 
 // Get route to get a single song by name
 router.get(
     "/get/songname/:songName",
-    passport.authenticate("jwt", {session: false}),
+    passport.authenticate("jwt", { session: false }),
     async (req, res) => {
-        const {songName} = req.params;
+        const { songName } = req.params;
 
-        const songs = await Song.find({name: songName}).populate("artist");
-        return res.status(200).json({data: songs});
+        const songs = await Song.find({ name: songName }).populate("artist");
+        return res.status(200).json({ data: songs });
     }
 );
 
